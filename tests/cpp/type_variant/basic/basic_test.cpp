2091c456c75fd8ecd9530a6d2daac50ca2818c19
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2024-07-17 11:25:45 +0200

    Working on the tests

--- tests/cpp/type_variant/basic/basic_test.cpp
+++ tests/cpp/type_variant/basic/basic_test.cpp
@@ -5,6 +5,22 @@ int main() {
   auto value1 = test::DataType::from_values(1);
   auto value2 = test::DataType::from_values((float)1.5);
 
+  if (value1 != value1) {
+    return 1;
+  }
+  if (value1 == test::DataType::from_values(2)) {
+    return 2;
+  }
+  if (value2 != value2) {
+    return 3;
+  }
+  if (value2 == test::DataType::from_values((float)2.5)) {
+    return 4;
+  }
+  if (value1 == value2) {
+    return 5;
+  }
+
   std::cout << "Done" << std::endl;
 
   return 0;
