361d8c6a31f775e34e5d7ade08b18184ae4a0c9f
Rasmus <nielsen@beamfox.dk>
2025-10-27 14:45:33 +0100

    Fixed a documentation typo

--- src/dart/mod.rs
+++ src/dart/mod.rs
@@ -5,8 +5,8 @@
 //!
 //! For any data model to work the termite dependency must be generated from
 //! get_termite_dependency() and be saved as "termite.dart" and
-//! "termite_types.dart" at a location where they can be included as "import
-//! 'termite.dart';" and "import 'termite_types.dart';"
+//! "termite-types.dart" at a location where they can be included as "import
+//! 'termite.dart';" and "import 'termite-types.dart';"
 //!
 
 use std::collections::{HashMap, HashSet};
@@ -22,7 +22,7 @@ mod type_struct;
 mod type_variant;
 
 /// Obtains the base termite Dart dependencies required for all generated data
-/// models, must be saved as "termite.dart" and "termite_types.dart"
+/// models, must be saved as "termite.dart" and "termite-types.dart"
 pub fn get_termite_dependency() -> (&'static str, &'static str) {
     return (
         include_str!("termite.dart"),
